8e175166aebaac2c95121a5c9efac4b6e894b6f7
Elena Chaikouskaya <dolika0ch@gmail.com>
2024-10-06 13:14:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ print(spisok)#вывод списка
 
     
 
+#length = int(input("Введите колво элементов списка: "))
+#numb =[0]*length
+#for i in range (0,length ):
+  #  numb [i] = int(input("Введите число: "))
+#numbers_2=[0]*length
+
+#for i in range(0,length):
+#    numbers_2[i] = pow(numb [i],2)
+#print(numbers_2)
+
 
 
 
